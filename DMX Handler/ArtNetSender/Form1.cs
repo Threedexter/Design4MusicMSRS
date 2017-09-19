24bcbdb7c4853feeb44887ec5251e90bbea8eb8c
Unknown <mathieuvwunnik@planet.nl>
2017-09-19 14:45:53 +0200

    New sample program for mocking DMX 512
    -Form for single channel control (UDP)
    -Packet receiver (Console)

---
+++ DMX Handler/ArtNetSender/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace ArtNetSender
+{
+    public partial class Form1 : Form
+    {
+        byte[] _dmxData;
+        ArtNet.Sockets.ArtNetSocket artnet;
+
+        public Form1()
+        {
+            InitializeComponent();
+            artnet = new ArtNet.Sockets.ArtNetSocket();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (artnet == null || !artnet.PortOpen)
+                {
+                    artnet.EnableBroadcast = true;
+                    artnet.Open(IPAddress.Parse("127.0.0.1"), IPAddress.Parse("255.255.255.255"));
+                    Console.WriteLine(artnet.BroadcastAddress.ToString());
+                    Console.WriteLine("opened UDP connection...");
+                    _dmxData = new byte[512];
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_dmxData != null && artnet != null)
+            {
+                _dmxData[(int)numericUpDown1.Value] = Convert.ToByte(trackBar1.Value);
+                var packet = new ArtNet.Packets.ArtNetDmxPacket();
+                packet.DmxData = _dmxData;
+                artnet.Send(packet);
+            }
+            else
+            {
+                Console.WriteLine("uninitialized properties");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            artnet.
+        }
+    }
+}
