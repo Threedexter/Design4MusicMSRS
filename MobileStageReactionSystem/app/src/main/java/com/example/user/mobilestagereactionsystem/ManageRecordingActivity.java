a53f7d41a97d894783ca2bd45d1de6ff09367116
biepbot <biepbot@gmail.com>
2017-09-26 14:39:49 +0200

    Show gestures in list

--- MobileStageReactionSystem/app/src/main/java/com/example/user/mobilestagereactionsystem/ManageRecordingActivity.java
+++ MobileStageReactionSystem/app/src/main/java/com/example/user/mobilestagereactionsystem/ManageRecordingActivity.java
@@ -1,10 +1,17 @@
 package com.example.user.mobilestagereactionsystem;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
+import com.mock.Data;
 import com.modals.NavigationAppActivity;
+import com.modals.Navigator;
+
+import ext.gestureDetection.base.Gesture;
+
 
 /**
  * Created by fhict on 25/09/2017.
@@ -17,5 +24,17 @@ public class ManageRecordingActivity extends NavigationAppActivity {
         setContentView(R.layout.manage_recording_screen);
         super.onCreate(savedInstanceState);
 
+        ListView lv = (ListView) findViewById(R.id.lvRecordings);
+
+        ArrayAdapter<Gesture> ag = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Data.getGestures());
+        lv.setAdapter(ag);
+
+        Button add = (Button) findViewById(R.id.btAdd);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigator.moveView(ManageRecordingActivity.this, Record.class);
+            }
+        });
     }
 }
